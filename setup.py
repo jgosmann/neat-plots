efd6d5ebc69109a453621b312c62bc6ea273e634
Jan Gosmann <jan@hyper-world.de>
2013-04-04 17:19:37 +0200

    Package organization and setup script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='neatplots',
+    version='0.1',
+    description='Sane colors for matplotlib.',  # TODO better description
+    author='Jan Gosmann',
+    author_email='jan@hyper-world.de',
+    # url= ... TODO
+    packages=find_packages())
