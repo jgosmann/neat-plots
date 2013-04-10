675564b8fa4f324fc5c245f014fa9c8fcab08bdf
Jan Gosmann <jan@hyper-world.de>
2013-04-10 16:20:15 +0200

    Some more palettes with different number of colors

--- neatplots/predefined.py
+++ neatplots/predefined.py
@@ -1,8 +1,12 @@
 from neatplots.colors import QualitativePalette
 
-
+poles2 = QualitativePalette(2, 15)
+pinkgrass = QualitativePalette(2, 160)
 summer = QualitativePalette(3, 0)
 earth = QualitativePalette(3, 60)
 warm = QualitativePalette(3, 6, 180)
 cold = QualitativePalette(3, 186, 180)
 poles = QualitativePalette(3, 237, 180)
+four = QualitativePalette(4, 30)
+five = QualitativePalette(5, 0)
+rainbow = QualitativePalette(6, 20)
