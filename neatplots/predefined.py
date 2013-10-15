a385582d3a5a744c72fefb5cc2be39dda123e3cc
Jan Gosmann <jan@hyper-world.de>
2013-10-15 15:18:50 +0200

    Use dict to sort palettes by color number.

--- neatplots/predefined.py
+++ neatplots/predefined.py
@@ -1,12 +1,28 @@
 from neatplots.colors import QualitativePalette
 
-poles2 = QualitativePalette(2, 15)
-pinkgrass = QualitativePalette(2, 160)
-summer = QualitativePalette(3, 240)
-earth = QualitativePalette(3, 60)
-warm = QualitativePalette(3, 6, 180)
-cold = QualitativePalette(3, 186, 180)
-poles = QualitativePalette(3, 237, 180)
-four = QualitativePalette(4, 30)
-five = QualitativePalette(5, 0)
-rainbow = QualitativePalette(6, 20)
+palettes = {
+    2: {
+        'poles2': QualitativePalette(2, 15),
+        'pinkgrass': QualitativePalette(2, 160)
+    },
+    3: {
+        'summer': QualitativePalette(3, 240),
+        'earth': QualitativePalette(3, 60),
+        'warm': QualitativePalette(3, 6, 180),
+        'cold': QualitativePalette(3, 186, 180),
+        'poles': QualitativePalette(3, 237, 180)
+    },
+    4: {
+        'four': QualitativePalette(4, 30)
+    },
+    5: {
+        'five': QualitativePalette(5, 0)
+    },
+    6: {
+        'rainbow': QualitativePalette(6, 20)
+    }
+}
+
+for d in palettes.itervalues():
+    for name, palette in d.iteritems():
+        vars()[name] = palette
