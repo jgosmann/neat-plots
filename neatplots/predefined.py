632dfcb7d420e84b2eb964b8cf96eb53e054d48b
Jan Gosmann <jan@hyper-world.de>
2013-10-15 15:16:02 +0200

    Summer palette adjustment.

--- neatplots/predefined.py
+++ neatplots/predefined.py
@@ -2,7 +2,7 @@ from neatplots.colors import QualitativePalette
 
 poles2 = QualitativePalette(2, 15)
 pinkgrass = QualitativePalette(2, 160)
-summer = QualitativePalette(3, 0)
+summer = QualitativePalette(3, 240)
 earth = QualitativePalette(3, 60)
 warm = QualitativePalette(3, 6, 180)
 cold = QualitativePalette(3, 186, 180)
